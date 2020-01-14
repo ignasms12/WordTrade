27c6a24e6edb51b0b778a6473d588a05fb2089c3
Vytenis domasevicius <vytenis.domasevicius@vka.lt>
2020-01-14 19:14:49 +0200

    Loading transitions fully working, changed the colour scheme

--- src/components/bookdetails.js
+++ src/components/bookdetails.js
@@ -10,6 +10,7 @@ import handshake from '../images/handshake.png';
 import whitechat from '../images/whitechat.png';
 import settings from '../images/settings-gears.svg';
 import noImg from '../images/noImg.svg';
+import loading from '../images/loading.gif';
 //var Description = "Aged thirteen, Theo Decker, son of a devoted mother and a reckless, largely absent father, survives an accident that otherwise tears his life apart. Alone and rudderless in New York, he is taken in by the family of a wealthy friend. He is tormented by an unbearable longing for his mother, and down the years clings to the thing that most reminds him of her: a small, strangely captivating painting that ultimately draws him into the criminal underworld. As he grows up, Theo learns to glide between the drawing rooms of the rich and the dusty antiques store where he works. He is alienated and in love - and his talisman, the painting, places him at the centre of a narrowing, ever more dangerous circle. The Goldfinch is a haunted odyssey through present-day America and a drama of enthralling power. Combining unforgettably vivid characters and thrilling suspense, it is a beautiful, addictive triumph - a sweeping story of loss and obsession, of survival and self-invention, of the deepest mysteries of love, identity and fate.";
 // TODO padaryt kad per urla gal paimtu some kind of book code
 // ir pagal tai is db pasiimtu pavadinima ir tada butu iskarto import {name} ir componentas done
@@ -66,7 +67,22 @@ export default class bookdetails extends Component {
         if (error) {
         return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
-        return <div>Loading...</div>;
+            return (
+                <React.Fragment>
+                    <body>
+                        <div className="loadingContainer">
+                            <img src={loading}/>
+                        </div>
+                        <footer>
+                            <Link to = "/wishlist"><div id="wishlist" className="navbar-element"><img src={whitelist}/><span>WishList</span></div></Link>
+                            <Link to = "/ownedlist"><div id="ownedlist" className="navbar-element"><img src={wishlistImg}/><span>OwnedList</span></div></Link>
+                            <Link to = "/deals"><div id="deals" className="navbar-element"><img src={handshake}/><span>Deals</span></div></Link>
+                            <Link to = "/messaging"><div id="messaging" className="navbar-element"><img src={whitechat}/><span>Messaging</span></div></Link>
+                            <Link to = "/settings"><div id="settings" className="navbar-element"><img src={settings}/><span>Settings</span></div></Link>
+                        </footer>
+                    </body>
+                </React.Fragment>
+            )
         } else {
             return (
                 <React.Fragment>
